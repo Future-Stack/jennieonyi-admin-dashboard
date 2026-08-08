af0b5cdb51925a2c9eea7a8a8aadd9b45265ff1c
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-08-08 07:51:14 +0600

    Implemented analytics page's heading and state cards

--- app/(admin)/dashboard/analytics/page.tsx
+++ app/(admin)/dashboard/analytics/page.tsx
@@ -1,7 +1,67 @@
+"use client"
+import AnalyticStateCard from "@/components/admin/analytics/AnalyticStateCard";
+import Headers from "@/components/admin/common/Headers";
+import { Download, RefreshCw } from "lucide-react";
+
+const statCardsData = [
+  {
+    title:"Total Revenue",
+    value:"$488,500",
+    change:"+12.4% vs last month",
+    isError:false,
+    positive:true
+  },
+  {
+    title:"Vendor Sales",
+    value:"$32,500",
+    change:"+8.2% vs last month",
+    isError:false,
+    positive:true
+  },
+  {
+    title:"Service Revenue",
+    value:"$643,500",
+    change:"+14.1% vs last month",
+    isError:false,
+    positive:true
+  },
+  {
+    title:"Total Bookings",
+    value:"1070",
+    change:"+9.5% vs last month",
+    isError:false,
+    positive:true
+  },
+  {
+    title:"Product Orders",
+    value:"5470",
+    change:"+9.5% vs last month",
+    isError:false,
+    positive:true
+  },
+]
+
 export default function AnalyticsPage() {
   return (
-    <div className="bg-white p-8 rounded-2xl shadow-sm border border-gray-100 min-h-[50vh] flex items-center justify-center">
-      <h2 className="text-2xl font-medium text-gray-400">Analytics & Reports Page</h2>
+    <div className="flex flex-col min-h-[calc(100vh-80px)] bg-white py-8 overflow-y-auto">
+      <Headers
+        title="Analytics & Reports"
+        description="Platform insights across services, vendors, bookings, and growth"
+        buttonOneIcon={<Download className="w-4 h-4" />}
+        buttonOneText="Export CSV"
+        buttonOneAction={() => { }}
+        buttonTwoIcon={<RefreshCw className="w-4 h-4" />}
+        buttonTwoText="Refresh"
+        buttonTwoAction={() => { }}
+      />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
+        {
+          statCardsData.map((card, index) => (
+            <AnalyticStateCard key={index} {...card} />
+          ))
+        }
+      </div>
     </div>
   );
 }
