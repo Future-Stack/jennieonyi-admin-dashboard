aac146638dd848d4abe52ca916608438f2ca8eaa
theashim-3 <ashim@softvence.com>
2026-08-11 10:28:10 +0600

    update all page in padding issue

--- app/(admin)/dashboard/stylists/page.tsx
+++ app/(admin)/dashboard/stylists/page.tsx
@@ -123,7 +123,7 @@ export default function StylistsPage() {
   };
 
   return (
-    <div className="flex flex-col min-h-[calc(100vh-80px)] bg-white p-8 overflow-y-auto">
+    <div className="flex flex-col min-h-[calc(100vh-80px)] bg-white overflow-y-auto">
       
       {/* Page Header */}
       <div className="flex items-center justify-between shrink-0 mb-6">
--- app/(admin)/dashboard/users/page.tsx
+++ app/(admin)/dashboard/users/page.tsx
@@ -131,7 +131,7 @@ export default function UsersPage() {
   };
 
   return (
-    <div className="flex flex-col min-h-[calc(100vh-80px)] bg-white p-8 overflow-y-auto">
+    <div className="flex flex-col min-h-[calc(100vh-80px)] bg-white overflow-y-auto">
       
       {/* Page Header */}
       <div className="flex items-center justify-between shrink-0 mb-8">
